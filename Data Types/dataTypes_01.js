a73ec6fa3e2227bc45ef3529d79e080647fc4d81
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-14 15:11:03 +0500

    Add files via upload
    Data Types Part 1.

---
+++ Data Types/dataTypes_01.js
@@ -0,0 +1,24 @@
+"use strict" ; // treat all JS code as newer version
+// alert(3+3) // we are using node js not browser
+console.log(3+
+    3) // code readability is important
+console.log("Farooque");    
+
+let name = "Farooque"
+let age = 21
+let isLogged = true
+let state ;
+
+// premitive data type
+
+// number => range --> 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone values
+// undefines => 
+// symbol => unique   
+
+console.log(typeof age);
+console.log(typeof undefined); // it's undefined
+console.log(typeof null); // it's object
