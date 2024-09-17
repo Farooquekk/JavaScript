dbd358b71c4f619495dda48aeaeca83b7760ca86
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-17 00:19:04 -0700

    Create variables.js

---
+++ Variables/variables.js
@@ -0,0 +1,23 @@
+const id = "22SW040"
+var email = "Farooquekk92@gmail.com"
+let name = "Farooque"
+uniName = "Muet"
+let gpa 
+console.log(id)
+console.log(email)
+console.log(name)
+console.log(uniName);
+console.log(gpa)
+console.log(" ");
+console.log(id+" "+email+" "+name+" "+uniName);
+
+// id = "22SW037" // it will give you error b/c const is not going to be change
+
+email="farooquekk92@gmail.com"
+name="Farooque Sajjad"
+uniName = "Muet Jamshoro"
+
+console.table(id + name + email + uniName )
+console.table([id , name , email , uniName , gpa] )
+
+// Note : prefer not to use var b/c of issue in block scope and functional scope;
