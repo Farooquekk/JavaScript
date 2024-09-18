5030e1e0d1a51658490c9cf5ec3d9b9ab9375d2e
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-18 04:58:39 -0700

    Create string.js

---
+++ String/string.js
@@ -0,0 +1,27 @@
+const name = "Farooque"
+const repoCount = 6
+//console.log(name+repoCount+" Values");// not recommended b/c it's outdated
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const name2 = new String("farooque")
+// console.log(name2[5]);
+// console.log(name2.__proto__);
+// console.log(name2.length);
+// console.log(name2.toUpperCase());
+// console.log(name2.charAt(3));
+// console.log(name2.indexOf('f'));
+
+// const newString = name2.substring(0,4);
+// console.log(newString); 
+// const newString1 = name2.slice(-8,5);
+// console.log(newString1); 
+
+// const newName = "   Sajjad"
+// console.log(newName);
+// console.log(newName.trim());
+
+const url = "https://Farooque/Sajjad%20.com"
+// console.log(url.replace("%20","_"));
+// console.log(url.includes("Far"));
+// console.log(url.includes("kk"));
+console.log(url.split("/"));
