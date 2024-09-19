4d578db560a32fa723ee030adca3fc30f1509fbf
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-19 05:59:59 -0700

    Create nums and math.js

---
+++ Nums And Math/nums and math.js
@@ -0,0 +1,31 @@
+// const score = 100
+// console.log(score);
+// const balance = new Number(1000)
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(9));
+
+// const number = 123.8966
+// console.log(number.toPrecision(3));
+
+// const number1 = 100000
+// console.log(number1.toLocaleString());
+// console.log(number1.toLocaleString('en-IN'));
+
+// *************** Maths *****************
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(2,3,10,11,4));
+// console.log(Math.min(2,3,10,11,4));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10 + 1); // we usually avoid this 
+console.log((Math.random()*10) + 1); // prefer this 
+const min = 10
+const max = 20
+console.log( Math.floor((Math.random()*(max-min+1) )+ min));
