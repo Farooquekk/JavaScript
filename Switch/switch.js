fe5f52b5c043574f8590b05312ec059441de72cf
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-20 04:56:43 -0700

    Create switch.js

---
+++ Switch/switch.js
@@ -0,0 +1,31 @@
+// Switch Statement in JS
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//const month = 3
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "fab":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Case Match");
+        break;
+}
