b83e87ef3ba53372e5846a98fbef9ab7973efff3
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-21 04:56:51 -0700

    Create comparison.js

---
+++ Comparison/comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+// please avoid these types of conversions
+
+
+// ===   =>  strict conversion -> comapre value as well as check data type also --> must be same data type tp pass (===) condition
+
+console.log("2"==2);
+console.log(2==2);
+console.log(2===2);
+console.log("2"===2);
