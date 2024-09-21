adc9b59a28c1c597bb599500ebfa8835efaf5b2e
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-21 04:54:02 -0700

    Create array_02.js

---
+++ Arrays/array_02.js
@@ -0,0 +1,35 @@
+const marvel_heros = ["Thor","Ironman","Hulk"]
+const dc_heros = ["Superman","Batman","Spiderman"]
+
+// console.log(marvel_heros);
+// console.log(dc_heros);
+
+// console.log(marvel_heros.push(dc_heros));
+// console.log(marvel_heros);
+
+// const all_heros = marvel_heros.concat(dc_heros)
+// console.log(all_heros);
+
+
+// const all_new_heros = [...marvel_heros,...dc_heros] // spread operator --> like drop a glass 
+// console.log(all_new_heros);
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// console.log(another_array);
+// const real_another_array = another_array.flat(Infinity);
+
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Farooque"));
+// console.log(Array.from("Farooque"));
+// console.log(Array.from({name:"Farooque"})); // interesting
+
+const s1 = 100
+const s2 = 200
+const s3 = 300
+
+console.log(Array.of(s1,s2,s3));
+
+
+
+
