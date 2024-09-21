6577a35185d7c0ad516c376ab8710b492d8f39f8
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-21 04:55:46 -0700

    Create conversion operation.js

---
+++ Conversion Operation/conversion operation.js
@@ -0,0 +1,89 @@
+// let score = 33;
+// //req.body
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score1 = "33"
+// let valueInNumber = Number(score1)
+// console.log(typeof valueInNumber)
+// console.log(typeof score1);
+
+// let score2 = "33abcd"
+// let valueInNumber1 = Number(score2)
+// console.log(typeof valueInNumber1)
+
+// let score3 = "Farooque"
+// let valueInNumber2 = Number(score3)
+// console.log(typeof valueInNumber2)
+
+// let score4 = null
+// let valueInNumber3 = Number(score4)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3);
+
+// let score5 = undefined
+// let valueInNumber4 = Number(score5)
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4);
+
+// let score6 = false
+// let valueInNumber5 = Number(score6)
+// console.log(typeof valueInNumber5);
+// console.log(valueInNumber5);
+
+// "33" => 33 -> number
+// "33abc" => -> NaN but still a number
+// true => 1, false => 0
+// 
+
+// let isLoggedIn = -999
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+
+// let name = "F"
+// let stringName = Boolean(name)
+// console.log(stringName);
+// console.log(typeof(stringName));
+
+// 1 => true, 0 => false 
+// "" => false, "Farooque" => true
+
+// ************* Operations ***************
+
+// let value = 10
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // ** is for power in js
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Farooque"
+// let str2 = "Sajjad"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+"F");
+// console.log("F"+1);
+// console.log("F"+1+3);
+// console.log(1+3+"F");
+
+// console.log(2+2*5%3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let n1, n2, n3
+n1 = n2 = n3 = 2 + 2 // don't recommended b/c of low readability
+
+let v = 10,u=10
+console.log("V = " + v++)
+console.log("U = "+ ++u)
+
+console.log(v);
