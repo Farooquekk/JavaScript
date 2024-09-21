0c6a3af648b4d058c042bf04bc1bf457dbcbd1f2
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-21 05:05:27 -0700

    Create even odd.js

---
+++ Even or Odd Program/even odd.js
@@ -0,0 +1,9 @@
+function evenOdd(num) {
+    if (num % 2 == 0) console.log(`The given number ${num} is even`);
+    else console.log(`The given number ${num} is odd`);
+}
+
+evenOdd(22);
+evenOdd(11);
+
+console.log();  // Output will appear between each function call
