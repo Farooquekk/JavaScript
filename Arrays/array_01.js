e11cee2d3bcb456a13d282e6abb2f5fee1890b73
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-21 04:53:24 -0700

    Create array_01.js

---
+++ Arrays/array_01.js
@@ -0,0 +1,40 @@
+// Arrays in js 
+
+// const array = [0,1,2,3,4,5,6]
+// console.log(array);
+
+// const myHero = ["Thor","Ironman","Hulk"]
+// console.log(myHero[1]);
+
+const array2 = new Array(11,12,13,14,151,6) 
+// console.log(array2[5]);
+
+// array2.push(120)
+// array2.push(-199)
+// console.log(array2);
+
+// console.log(array2.pop());
+// console.log(array2);
+
+// console.log(array2.unshift(-200));
+// console.log(array2);
+// console.log(array2.shift());
+// console.log(array2);
+
+// console.log(array2.includes(11));
+// console.log(array2.includes(-1));
+
+// console.log(array2.indexOf(151));
+// console.log(array2.indexOf(-1));
+
+// const newArray = array2.join()
+// console.log("String --> " + newArray);
+// console.log("Array --> " + array2);
+
+console.log("A",array2);
+console.log(array2.slice(1,3));
+
+console.log("B",array2);
+
+console.log(array2.splice(1,3));
+console.log("C",array2);
