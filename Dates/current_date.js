b716afdd8be6b933fa05f9f5b44906605d446bae
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-22 05:27:30 -0700

    Create current_date.js

---
+++ Dates/current_date.js
@@ -0,0 +1,18 @@
+let dates = new Date();
+
+
+
+// console.log(dates.toUTCString());
+// console.log(dates.toDateString());
+// console.log(dates.toUTCString());
+// console.log('****************************');
+// console.log(dates.getTime());
+// console.log(dates.getFullYear());
+// console.log(dates. getMonth());
+// console.log(dates.getDate());
+// console.log(dates.getHours()  );
+// console.log(dates.toISOString());
+// console.log(dates.toJSON());
+
+let currentDate = Date.now();
+console.log(currentDate);
