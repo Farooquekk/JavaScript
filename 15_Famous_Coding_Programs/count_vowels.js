00270f93dbc68777100c037449352877a22cc0e0
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-29 06:17:04 -0700

    Create count_vowels.js

---
+++ 15_Famous_Coding_Programs/count_vowels.js
@@ -0,0 +1,13 @@
+function countVowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels("Hello World")); 
