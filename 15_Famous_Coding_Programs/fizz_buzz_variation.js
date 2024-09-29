57ac738474dd25dd84677dea12cfa383a324e9fb
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-29 06:19:10 -0700

    Create fizz_buzz_variation.js

---
+++ 15_Famous_Coding_Programs/fizz_buzz_variation.js
@@ -0,0 +1,15 @@
+function fizzBuzzRange(start, end) {
+    for (let i = start; i <= end; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzzRange(1, 30);
